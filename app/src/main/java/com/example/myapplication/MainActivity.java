17532991fdf0c8fd9e708dbd85d8879e06b8cc3a
aGith0bszkola <githubszkola@gmail.com>
2026-03-23 14:30:32 +0100

    na 4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> jezykiArrayList;
     private ArrayAdapter<String> arrayAdapter;
     private ListView listView;
+    private EditText editText;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 view.setBackgroundColor(Color.GRAY);
                 jezykiArrayList.remove(i);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+        editText = findViewById(R.id.editTextJezyk);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String jezyk = editText.getText().toString();
+                jezykiArrayList.add(jezyk);
                 arrayAdapter.notifyDataSetChanged();
             }
         });
