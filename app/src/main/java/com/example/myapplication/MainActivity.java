7da0f8a74fec1ddbd2244989ec06a5697d68be3f
aGith0bszkola <githubszkola@gmail.com>
2026-03-23 14:23:12 +0100

    na 4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -31,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,jezykiArrayList);
         listView =  findViewById(R.id.ListViewJezyki);
         listView.setAdapter(arrayAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                view.setBackgroundColor(Color.GRAY);
+                jezykiArrayList.remove(i);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
 
     }
 }
